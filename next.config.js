e0fb4e99d5eac94fc04ab22794503bd129929457
chayangkoon <chayangkoondokhom@gmail.com>
2018-08-31 18:00:03 +0700

    start layout

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 const withSass = require('@zeit/next-sass')
 module.exports = withSass({
-  cssModules: true
+  sassLoaderOptions: {
+    includePaths: ["absolute/path/a", "absolute/path/b"]
+  }
 })
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
-import { Icon } from 'antd'
+import { Row,Col,Layout } from 'antd'
+import '../styles/index.scss'
+
+const { Header, Footer, Content } = Layout;
 
 class Index extends Component {
   render() {
     return (
-      <div>
-        <Icon type="loading" />
+      <div className="container">
+        <Layout className="layout">
+          <Header className="header">
+            <Row type="flex" justify="end">
+              <Col>Header</Col>
+            </Row>
+          </Header>
+          <Content>Content</Content>
+          <Footer>Footer</Footer>
+        </Layout>
       </div>
     );
   }
